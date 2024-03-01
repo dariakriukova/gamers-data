ee3a3920821f370d8e37f6e927a13fb1b282146f
Daria Kriukova <daria.kriukova@elfys.fi>
2024-03-01 10:41:54 +0200

    1. Sample files from the template removed. 2. Module-level constructs for the database defined.

--- sample/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Python Sample App - Main Module
-"""
-
-
-from .cli import cli
-
-if __name__ == "__main__":
-    cli()  # pylint: disable=no-value-for-parameter
--- src/database.py
+++ src/database.py
@@ -1,23 +1,44 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base, sessionmaker
+"""
+Database schema for user data loaded from "Wild Wild Chords" and "HarmonicaBots" games.
+"""
 
-Base = declarative_base()
+from sqlalchemy import ForeignKey, Date
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from datetime import date
+
+class Base(DeclarativeBase):
+    pass
+    
 
 class User(Base):
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String, unique=True)
-    gender = Column(String)
-    dob = Column(Date)
-    registration_date = Column(Date)
-    country_or_region = Column(String)
-    picture_url = Column(String)
-    nationality = Column(String)
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    gender: Mapped[str]
+    dob: Mapped[date] = mapped_column(Date)
+    registration_date: Mapped[date] = mapped_column(Date)
+    picture_url: Mapped[str]
+    nationality: Mapped[str]
+    region_id: Mapped[int] = mapped_column(ForeignKey("region.id"))
+    region: Mapped["Region"] = relationship(back_populates="children")
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, email={self.email!r})"
 
-engine = create_engine('sqlite:///wwc_hb.db')
+
+class Region(Base):
+    __tablename__ = 'region'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    users: Mapped[list["User"]] = relationship("User", back_populates="region")
+    
+engine = create_engine("sqlite:///wwc_hb.db", echo=True)
 Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
